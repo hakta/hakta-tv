5e94d2b3e39a0636c5cfc0a0127924c1d447a115
Victor Godoy Poluceno <victorpoluceno@gmail.com>
2012-03-17 14:31:59 -0300

    Add bootstrap and a new medir dir

--- app_socketio.js
+++ app_socketio.js
@@ -1,9 +1,9 @@
 var io = require('socket.io').listen(8080);
 
 var videoSourceList = [
-    "/media/big_buck_bunny_640x360_2.28.webm", 
-    "/media/sintel_640x360_2.28.webm",
-    "/media/elephants_dream_640x360_2.30.webm"
+    "/static/media/big_buck_bunny_640x360_2.28.webm", 
+    "/static/media/sintel_640x360_2.28.webm",
+    "/static/media/elephants_dream_640x360_2.30.webm"
 ];
 
 io.sockets.on('connection', function (socket) {
