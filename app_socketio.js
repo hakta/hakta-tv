c2a91c7570f843465d40708a1c887292d3b6f0d4
Victor Godoy Poluceno <victorpoluceno@gmail.com>
2012-03-18 09:41:54 -0300

    Stable version

--- app_socketio.js
+++ app_socketio.js
@@ -1,9 +1,9 @@
 var io = require('socket.io').listen(8080);
 
 var videoSourceList = [
-    "/static/media/big_buck_bunny_640x360_2.28.webm", 
-    "/static/media/sintel_640x360_2.28.webm",
-    "/static/media/elephants_dream_640x360_2.30.webm"
+    "/static/media/big_buck_bunny_640x360_2.28", 
+    "/static/media/sintel_640x360_2.28",
+    "/static/media/elephants_dream_640x360_2.30"
 ];
 
 io.sockets.on('connection', function (socket) {
--- static/js/hydra.js
+++ static/js/hydra.js
@@ -1,32 +1,53 @@
-var playList;
-
-function play(){
-    player = $('#videoPlayer')[0];
-    source = $('#videoSource')[0];    
-
-    source.src = playList[0];
+function set_current(path){
+    var extension = Modernizr.video.h264 ? 'mp4': 
+                    Modernizr.video.webm ? 'webm': 
+                                           'mp4';    
+    current = path;
+    source.src = path + "." + extension;
+    source.type = "video/" + extension;
     player.load();
-    player.play();
+}
 
-    player.onended = function(){
-        var current = playList.indexOf(parseUri(source.src).path);
-        if (current == playList.lenght){
-            source.src = playList[0];
-        } else {
-            source.src = playList[current+1];
-        }
-        
-        player.load();
-        player.play();
+function first(){
+    set_current(playList[0])
+    player.play();
+}
+
+function next(){
+    var index = playList.indexOf(current);
+    if (index == playList.length-1){ // end of play list?
+        path = playList[0];
+    } else {
+        // next video
+        path = playList[index+1];
+    }
+
+    set_current(path);
+    player.play();    
+}
+
+function set(data){
+    // dont net to update if is same play list
+    if (!$(playList).compare(data['list'])) {
+        playList = data['list'];
+        first();
     }
 }
 
 function main(uri){
+    playList = null;
+    current = null;
+
+    player = $('#videoPlayer')[0];
+    source = $('#videoSource')[0];    
+
     var socket = io.connect(uri);
     socket.on('set', function (data) {
-        if (!$(playList).compare(data['list'])) {
-            playList = data['list'];
-            play();
-        }            
+        set(data);
     });
+
+    player.onended = function(){
+        // play next vido
+        next();
+    }
 };
